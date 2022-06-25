36e604a1beec24ee3b8206685d421182608d185f
Olivia Nwose <olivianwose@gmail.com>
2022-06-25 11:45:48 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const body = document.body
+const div = document.createElement("div")
+div.innerText="Olivia Nwose"
+body.append(div)
+
+
+const height = document.createElement("height")
+height.innerText="5ft 6inches"
+body.append(height)
+
+const p = document.createElement("p")
+p.innerText="Nigerian"
+body.append(p)
